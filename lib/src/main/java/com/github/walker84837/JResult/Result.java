154a4c8d37684e27e928a7517e9c739f8a128279
winlogon <walker84837@gmail.com>
2025-01-07 23:50:36 +0100

    docs: add javadocs to functions and class

--- lib/src/main/java/com/github/walker84837/JResult/Result.java
+++ lib/src/main/java/com/github/walker84837/JResult/Result.java
@@ -3,28 +3,78 @@ package com.github.walker84837.JResult;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+/**
+ * JResult - Result type that can be either an Ok variant or an Err variant.
+ * 
+ * @param <T> The type of the Ok variant.
+ * @param <E> The type of the Err variant.
+ */
 public abstract class Result<T, E> {
     public abstract boolean isOk();
     public abstract boolean isErr();
+
+    /**
+     * Unwraps the value of the Ok variant of the Result.
+
+     * @return The value of the Ok variant of the Result.
+     * @throws IllegalStateException If the Result is an Err variant.
+     */
     public abstract T unwrap() throws IllegalStateException;
+
+    /**
+     * Unwraps the error of the Err variant of the Result.
+
+     * @return The error of the Err variant of the Result.
+     * @throws IllegalStateException If the Result is an Ok variant.
+     */
     public abstract E unwrapErr() throws IllegalStateException;
+
+    /**
+     * Unwraps the value of the Ok variant of the Result, or returns a default value if the Result is an Err variant.
+
+     * @param defaultValue The default value to return if the Result is an Err variant.
+     * @return The value of the Ok variant of the Result, or the default value if the Result is an Err variant.
+     */
     public abstract T unwrapOr(T defaultValue);
     public abstract T unwrapOrElse(Function<E, T> fallback);
 
+    /**
+     * Creates a new Ok variant of the Result.
+
+     * @param value The value to wrap.
+     * @return A new Ok variant of the Result.
+     */
     public static <T, E> Result<T, E> ok(T value) {
         return new Ok<>(value);
     }
 
+    /**
+     * Creates a new Err variant of the Result.
+
+     * @param error The error to wrap.
+     * @return A new Err variant of the Result.
+     */
     public static <T, E> Result<T, E> err(E error) {
         return new Err<>(error);
     }
 
+    /**
+     * Executes the provided action if the Result is an Ok variant.
+
+     * @param action The action to execute.
+     */
     public abstract void ifOk(Consumer<T> action);
+
+    /**
+     * Executes the provided action if the Result is an Err variant.
+ 
+     * @param action The action to execute.
+     */
     public abstract void ifErr(Consumer<E> action);
 
     /**
      * Maps the value of the Ok variant of the Result.
-     * 
+
      * @param mapper The function to map the value with.
      * @return A new Result with the mapped value.
      */
@@ -36,6 +86,12 @@ public abstract class Result<T, E> {
         }
     }
 
+    /**
+     * Maps the error of the Err variant of the Result.
+
+     * @param mapper The function to map the error with.
+     * @return A new Result with the mapped error.
+     */
     public <F> Result<T, F> mapErr(Function<E, F> mapper) {
         if (this instanceof Err<T, E> err) {
             return Result.err(mapper.apply(err.error));
--- lib/src/main/java/com/github/walker84837/JResult/ResultUtils.java
+++ lib/src/main/java/com/github/walker84837/JResult/ResultUtils.java
@@ -2,7 +2,16 @@ package com.github.walker84837.JResult;
 
 import java.util.concurrent.Callable;
 
+/**
+ * Utility class for creating Results from Callables that may throw Exceptions.
+ */
 public class ResultUtils {
+    /**
+     * Creates a Result from a Callable that may throw an Exception.
+
+     * @param callable The Callable to create the Result from.
+     * @return The Result created from the Callable.
+     */
     public static <T> Result<T, Exception> tryCatch(Callable<T> callable) {
         try {
             return Result.ok(callable.call());
