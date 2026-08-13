a7645bc97eac229203d617f8b4cb7fd3897fa4e0
walker84837 <walker84837@users.noreply.github.com>
2026-08-13 20:28:14 +0000

    deploy: 87ee42ae1bf9a25447acaf74aaf86bbf523c02ed

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.walker84837.JResult"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.winlogon.jresult"}];updateSearchResults();
